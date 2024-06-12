77de3bf2d1b719df10734f4ad5a4e8bdf925c333
Simon Stein <sstein@fi.uba.ar>
2024-06-12 18:34:08 -0300

    update: add smooth animation to theme switch

--- src/components/layout/ThemeSwitcher.tsx
+++ src/components/layout/ThemeSwitcher.tsx
@@ -3,22 +3,37 @@ import { Button } from "@nextui-org/react";
 import { Sun1, Moon } from "iconsax-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
 
   return (
-    <div className="flex justify-end mr-5 mt-5">
-      <Button
-        isIconOnly
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      >
-        {theme === "dark" ? (
-          <Sun1 size="24" variant="Bold" />
-        ) : (
-          <Moon size="24" variant="Bold" />
+    <div className="flex justify-end mr-5 mt-5 absolute right-3">
+      <AnimatePresence>
+        {mounted && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Button
+              isIconOnly
+              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            >
+              {theme === "dark" ? (
+                <Sun1 size="24" variant="Bold" />
+              ) : (
+                <Moon size="24" variant="Bold" />
+              )}
+            </Button>
+          </motion.div>
         )}
-      </Button>
+      </AnimatePresence>
     </div>
   );
 }
