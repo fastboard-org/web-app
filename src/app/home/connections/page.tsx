c0919464301fec20cd45b1ce2e868417ff59e6f7
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-12 13:22:46 -0300

    add next-theme and theme switcher to change between light and dark mode

--- src/app/home/connections/page.tsx
+++ src/app/home/connections/page.tsx
@@ -1,7 +1,15 @@
+import { Card, CardBody } from "@nextui-org/react";
+
 export default function Connections() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <h2 className="text-4xl text-black">Connections</h2>
+      <h2 className="text-4xl">Connections</h2>
+      <b className="my-10" />
+      <Card>
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+      </Card>
     </main>
   );
 }
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,9 +1,15 @@
 import HomeNavbarLayout from "@/components/layout/HomeNavbarLayout";
+import { ThemeSwitcher } from "@/components/layout/ThemeSwitcher";
 
 export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <HomeNavbarLayout>{children}</HomeNavbarLayout>;
+  return (
+    <HomeNavbarLayout>
+      <ThemeSwitcher />
+      {children}
+    </HomeNavbarLayout>
+  );
 }
