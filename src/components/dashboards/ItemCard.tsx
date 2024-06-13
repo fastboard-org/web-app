c07ec34c71f38d4a6b0bf8b8dc43d10fa0c0359d
Simon Stein <sstein@fi.uba.ar>
2024-06-13 20:44:32 -0300

    feat: refactor DashboardCard into ItemCard

--- src/components/dashboards/ItemCard.tsx
+++ src/components/dashboards/ItemCard.tsx
@@ -1,72 +1,36 @@
-import { DashboardInterface } from "@/types";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@nextui-org/react";
-import { Copy, Edit, Folder2, More, Trash } from "iconsax-react";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import CardActions from "@/components/dashboards/CardActions";
 
-const Actions = () => {
+const ItemCard = ({
+  name,
+  icon,
+  className,
+  onClick,
+}: {
+  name: string;
+  icon?: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+}) => {
   return (
-    <Popover placement={"right"}>
-      <PopoverTrigger>
-        <More className={"cursor-pointer hover:text-foreground-500"} />
-      </PopoverTrigger>
-      <PopoverContent>
-        <div className=" flex flex-col p-2 gap-1">
-          <Button
-            className={
-              "bg-transparent text-left justify-between gap-5 hover:bg-opacity-10 hover:bg-foreground"
-            }
-            fullWidth
-            endContent={<Edit className={"text-foreground-400"} />}
-          >
-            Edit
-          </Button>
-          <Button
-            className={
-              "bg-transparent text-left justify-between gap-5 hover:bg-opacity-10 hover:bg-foreground"
-            }
-            fullWidth
-            endContent={<Copy className={"text-foreground-400"} />}
-          >
-            Duplicate
-          </Button>
-
-          <Button
-            className={
-              "bg-transparent text-left justify-between gap-5 hover:bg-opacity-10 hover:bg-foreground"
-            }
-            fullWidth
-            endContent={<Trash className={"text-danger"} />}
-          >
-            Delete
-          </Button>
-        </div>
-      </PopoverContent>
-    </Popover>
-  );
-};
-
-const ItemCard = ({ item }: { item: DashboardInterface  || FolderInterface}) => {
-  return (
-    <Card className={"w-[300px] h-[200px]"}>
+    <Card
+      className={"w-[300px] h-[200px] cursor-pointer  " + className}
+      isHoverable
+      isPressable
+      onClick={onClick}
+    >
       <CardHeader className={"p-5 flex gap-2"}>
-        {dashboard.isFolder && <Folder2 className={"text-primary"} size={20} />}
+        {icon}
         <h3
           className={
             "text-2xl whitespace-nowrap overflow-hidden overflow-ellipsis"
           }
         >
-          {dashboard.name}
+          {name}
         </h3>
       </CardHeader>
       <CardBody className={"flex-row-reverse justify-between items-end p-3"}>
-        <Actions />
+        <CardActions />
       </CardBody>
     </Card>
   );
