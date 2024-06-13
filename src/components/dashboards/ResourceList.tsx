ee4cd46a6a5f44c11c9e8aac0af4388591396b81
Simon Stein <sstein@fi.uba.ar>
2024-06-13 20:45:03 -0300

    feat: refactor DashboardList into ResourceList

--- src/components/dashboards/ResourceList.tsx
+++ src/components/dashboards/ResourceList.tsx
@@ -1,32 +1,51 @@
 "use client";
 import { DashboardInterface, FolderInterface } from "@/types";
 import ItemCard from "@/components/dashboards/ItemCard";
+import { Element, Folder2 } from "iconsax-react";
 
-const DashboardList = ({
+const ResourceList = ({
   dashboards,
   folders,
   search,
+  onFolderClick = (folder: FolderInterface) => {},
+  onDashboardClick,
 }: {
   dashboards: DashboardInterface[];
   folders: FolderInterface[];
   search: string;
+  onFolderClick?: (folder: FolderInterface) => void;
+  onDashboardClick: (dashboard: DashboardInterface) => void;
 }) => {
-  const getItemsToRender = (): {
-    dashboards: DashboardInterface[];
-    folders: FolderInterface[];
-  } => {};
-
   return (
     <section className={"flex flex-wrap w-full h-full gap-10 mt-5 "}>
+      {folders
+        .filter((folder) =>
+          folder.name.toLowerCase().includes(search.toLowerCase()),
+        )
+        .map((folder) => (
+          <ItemCard
+            key={folder.id}
+            name={folder.name}
+            icon={<Folder2 className={"text-primary"} size={20} />}
+            onClick={() => onFolderClick(folder)}
+          />
+        ))}
       {dashboards
-        .filter((dashboard) =>
-          dashboard.name.toLowerCase().includes(search.toLowerCase()),
+        .filter(
+          (dashboard) =>
+            dashboard.name.toLowerCase().includes(search.toLowerCase()) &&
+            !dashboard.folder_id,
         )
         .map((dashboard) => (
-          <ItemCard key={dashboard.id} name={dashboard.name} />
+          <ItemCard
+            key={dashboard.id}
+            name={dashboard.name}
+            icon={<Element className={"text-primary"} size={20} />}
+            onClick={() => onDashboardClick(dashboard)}
+          />
         ))}
     </section>
   );
 };
 
-export default DashboardList;
+export default ResourceList;
