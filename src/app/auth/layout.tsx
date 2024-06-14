93d02ad5c8cddbe2df5d1b3257a9741731bad8d7
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-14 02:16:50 -0300

    feat: add singup image

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,7 +10,7 @@ export default function LogInLayout({
 }>) {
   return (
     <div className="flex min-h-screen flex-row">
-      <div className="flex-1 light bg-background flex flex-col justify-center items-center">
+      <div className="flex-1 bg-background flex flex-col justify-center items-center">
         <div className="flex flex-row justify-center p-5 w-full">
           <Logo classname="text-primary" />
           <Spacer x={2} />
@@ -18,14 +18,12 @@ export default function LogInLayout({
         </div>
         {children}
       </div>
-      <div className="flex-1 flex flex-col justify-center items-center bg-content3">
+      <div className="flex-1 flex flex-col justify-center items-center bg-content2">
         <Image
-          width={300}
-          alt="NextUI hero Image"
-          src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+          sizes="2xl"
+          alt="Fastboard SingUp Image"
+          src="../SingUpImage.png"
         />
-        <Spacer y={5}></Spacer>
-        <h1 className="text-4xl">Fastboard</h1>
       </div>
     </div>
   );
