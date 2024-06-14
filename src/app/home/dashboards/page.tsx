f986d2a4ec6991d26d4accfea69a44445388c13b
Simon Stein <sstein@fi.uba.ar>
2024-06-14 14:19:16 -0300

    feat: add Breadcrumbs to dashboards page title

--- src/app/home/dashboards/page.tsx
+++ src/app/home/dashboards/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Input } from "@nextui-org/react";
+import { BreadcrumbItem, Breadcrumbs, Input } from "@nextui-org/react";
 import { Back } from "iconsax-react";
 import { CiSearch } from "react-icons/ci";
 import AddDashboardButton from "@/components/dashboards/AddDashboardButton";
@@ -29,32 +29,25 @@ export default function Dashboards() {
     //TODO redirect to editor
   };
 
+  const handleBack = () => {
+    setFolderSelected(null);
+  };
+
   return (
     <main className="flex min-h-screen flex-col p-16 w-full h-full gap-4">
-      <h2 className="text-[40px] flex items-center">
-        Dashboards{" "}
-        <AnimatePresence>
-          {folderSelected && (
-            <motion.span
-              className={"ml-2.5 flex items-center gap-3"}
-              initial={{ opacity: 0, x: 20 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 20 }}
-              transition={{ duration: 0.2 }}
-            >
-              {" "}
-              | {folderSelected.name}
-              <Back
-                className={
-                  "cursor-pointer text-primary hover:text-primary-300 hover:scale-90 transition-transform transition-colors"
-                }
-                size={40}
-                onClick={() => setFolderSelected(null)}
-              />
-            </motion.span>
-          )}
-        </AnimatePresence>
-      </h2>
+      <Breadcrumbs
+        itemClasses={{
+          item: "text-[40px]",
+          separator: "text-[40px]",
+        }}
+      >
+        <BreadcrumbItem onClick={handleBack}>
+          <h2>Dashboards</h2>
+        </BreadcrumbItem>
+        {folderSelected && (
+          <BreadcrumbItem>{folderSelected.name}</BreadcrumbItem>
+        )}
+      </Breadcrumbs>
       <div
         className={"flex justify-between w-full items-center gap-8 flex-wrap"}
       >
