c7a73c8ad2e0874138be353380fac9dc043b0218
Julian <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-15 16:04:40 -0300

    SingUp page first interaction (#7)
    * add singup layout and page
    
    * update: change light colors
    
    * update: added logo in page
    
    * feat: add singup image
    
    * update: add logic to change image depending on selected theme
    
    * Dashboards page first iteration (#6)
    
    * update: tailwind config colors
    
    * update: change font to Poppins
    
    * feat: create Logo component
    
    * feat: create CustomSkeleton component
    
    * update: navbar buttons styles
    
    * update: theme switcher styles
    
    * feat: start /home/dashboards page structure
    
    * feat: create DashboardInterface
    
    * feat: create useDashboards hook
    
    * feat: add react icons dependency
    
    * feat: add FolderInterface and update DashboardInterface
    
    * fix: update colorInterpolationFilters props in Logo component
    
    * feat: add folders property to useDashboards hook
    
    * feat: extract CardActions component into its own file
    
    * feat: refactor DashboardCard into ItemCard
    
    * feat: refactor DashboardList into ResourceList
    
    * feat: add folder click logic
    
    * feat: add Breadcrumbs to dashboards page title
    
    * add singup layout and page
    
    * update: change light colors
    
    * update: added logo in page
    
    * feat: add singup image
    
    * update: add logic to change image depending on selected theme
    
    ---------
    
    Co-authored-by: Simón Stein <86434696+stein257@users.noreply.github.com>

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { ThemeSwitcher } from "@/components/layout/ThemeSwitcher";
+import Logo from "@/components/layout/shared/Logo";
+import { Image, Spacer } from "@nextui-org/react";
+import { useTheme } from "next-themes";
+
+export default function LogInLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { theme } = useTheme();
+
+  return (
+    <div className="flex min-h-screen flex-row">
+      <div className="flex-1 bg-background flex flex-col justify-center items-center">
+        <div className="flex flex-row justify-center p-5 w-full">
+          <Logo classname="text-primary" />
+          <Spacer x={2} />
+          <h1 className="font-bold text-primary text-2xl">Fastboard</h1>
+        </div>
+        {children}
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center bg-content2">
+        <Image
+          sizes="2xl"
+          alt="Fastboard SingUp Image"
+          src={
+            theme === "light" ? "../SingUpImage.png" : "../SingUpImageDark.png"
+          }
+        />
+      </div>
+    </div>
+  );
+}
