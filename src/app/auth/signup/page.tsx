07bb77d08a18530b7dd82c2c72a6fc68a914b782
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-15 17:05:39 -0300

    feat: add react-form-hooks

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+import {
+  Button,
+  Card,
+  CardBody,
+  Divider,
+  Input,
+  Link,
+  Spacer,
+} from "@nextui-org/react";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoGithub } from "react-icons/io";
+import { Eye, EyeSlash } from "iconsax-react";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface SignUpFormInterface {
+  email: string;
+  password: string;
+}
+
+export default function SignUp() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormInterface>();
+
+  const onSubmit: SubmitHandler<SignUpFormInterface> = (signUpData) => {
+    console.log(signUpData);
+  };
+
+  return (
+    <main className="flex w-full flex-col items-center justify-center">
+      <Card className="w-full max-w-md p-4">
+        <CardBody>
+          <h1 className="text-4xl font-bold text-foreground">Get started</h1>
+          <Spacer y={2}></Spacer>
+          <h2 className="text-foreground">Create new account</h2>
+          <Spacer y={14}></Spacer>
+          <Button startContent={<FcGoogle size={24} />}>
+            Continue with Google
+          </Button>
+          <Spacer y={2}></Spacer>
+          <Button startContent={<IoLogoGithub size={23} />}>
+            Continue with Github
+          </Button>
+          <Divider className="my-4" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Please enter a valid email address",
+                },
+              })}
+              type="email"
+              label="Email"
+              errorMessage={errors.email?.message}
+              isInvalid={!!errors.email}
+              aria-label="Email"
+            />
+            <Input
+              {...register("password", {
+                required: "Password is required",
+              })}
+              label="Password"
+              aria-label="Password"
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => setPasswordVisible(!passwordVisible)}
+                >
+                  {passwordVisible ? <EyeSlash /> : <Eye />}
+                </button>
+              }
+              type={passwordVisible ? "text" : "password"}
+              errorMessage={errors.password?.message}
+              isInvalid={!!errors.password}
+            />
+            <Spacer y={4} />
+            <Button type="submit" color="primary" fullWidth>
+              Continue
+            </Button>
+          </form>
+        </CardBody>
+      </Card>
+      <Spacer y={2} />
+      <div className="flex flex-row">
+        <h3 className="text-foreground">Have an account?</h3>
+        <Spacer x={1}></Spacer>
+        <Link href="#">Login</Link>
+      </div>
+    </main>
+  );
+}
