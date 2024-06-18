f9b1a4628fc78bd2b893941e4c8832a52f1e9a08
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-18 20:32:51 -0300

    feat: add layout and component interface

--- src/atoms/editor/index.ts
+++ src/atoms/editor/index.ts
@@ -1,3 +1,4 @@
+import { DashboardMetadata, LayoutType } from "@/types/editor";
 import { atom } from "recoil";
 
 export const isComponentsDrawerOpen = atom({
@@ -7,4 +8,21 @@ export const isComponentsDrawerOpen = atom({
 export const isPropertiesDrawerOpen = atom({
   key: "isPropertiesDrawerOpen",
   default: false,
+});
+
+export const dashboardMetadata = atom<DashboardMetadata>({
+  key: "dashboardMetadata",
+  default: {
+    layouts: [
+      {
+        type: LayoutType.Full,
+        component1: {
+          type: "Image",
+          properties: {
+            src: "https://source.unsplash.com/random/800x600",
+          },
+        },
+      },
+    ],
+  },
 });
