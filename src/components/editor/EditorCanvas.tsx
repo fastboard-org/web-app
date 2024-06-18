753c9b20df89ebdfa7961cdc895f65c2a64b2939
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-18 20:41:28 -0300

    feat: add fastboard table component

--- src/components/editor/EditorCanvas.tsx
+++ src/components/editor/EditorCanvas.tsx
@@ -1,6 +1,7 @@
 import { isComponentsDrawerOpen, isPropertiesDrawerOpen } from "@/atoms/editor";
 import { motion } from "framer-motion";
 import { useRecoilValue } from "recoil";
+import FastboardTable from "./fastboard-components/FastboardTable";
 
 export default function EditorCanvas() {
   const isComponentsOpen = useRecoilValue(isComponentsDrawerOpen);
@@ -16,7 +17,7 @@ export default function EditorCanvas() {
       }}
       className="flex justify-center items-center h-full min-w-[75%] bg-background shadow-lg overflow-y-auto"
     >
-      Some content here
+      <FastboardTable />
     </motion.div>
   );
 }
