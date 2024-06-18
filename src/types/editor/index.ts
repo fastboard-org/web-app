5cb1ff1fc5dd415a60eb7874bb38187d460cb7ba
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-18 20:32:51 -0300

    feat: add layout and component interface

--- src/types/editor/index.ts
+++ src/types/editor/index.ts
@@ -2,3 +2,29 @@ export default interface PublishOption {
   label: string;
   description: string;
 }
+
+export enum LayoutType {
+  Full = "full",
+  Row = "row",
+}
+
+interface Layout {
+  type: LayoutType;
+}
+
+interface FullLayout extends Layout {
+  component1: FastboardComponent;
+}
+interface RowLayout extends Layout {
+  component1: FastboardComponent;
+  component2: FastboardComponent;
+}
+
+interface FastboardComponent {
+  type: string;
+  properties: Record<string, any>;
+}
+
+export interface DashboardMetadata {
+  layouts: Array<FullLayout | RowLayout>;
+}
