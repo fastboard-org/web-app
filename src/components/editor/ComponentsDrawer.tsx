6749620d63d628a434a18f1470e780a7cfdb32ae
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-20 17:57:22 -0300

    update: change component drawer style and remove draggeable component

--- src/components/editor/ComponentsDrawer.tsx
+++ src/components/editor/ComponentsDrawer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { isComponentsDrawerOpen } from "@/atoms/editor";
-import { Divider } from "@nextui-org/react";
+import { Divider, Spacer } from "@nextui-org/react";
 import { motion } from "framer-motion";
 import React from "react";
 import { useRecoilValue } from "recoil";
@@ -16,13 +16,16 @@ export default function ComponentsDrawer() {
       animate={{ x: isOpen ? 0 : "-100%" }}
       transition={{ ease: "easeInOut" }}
       className={
-        "h-full w-[22%] bg-background shadow-lg absolute top-0 left-0 z-10"
+        "h-full w-[22%] bg-background shadow-lg absolute top-0 left-0 z-10 p-5"
       }
     >
       <h3 className={"text-xl font-semibold p-5"}>Components</h3>
       <Divider />
-      <FastboardTableDraggeable />
-      <FastboardTableDraggeable2 />
+      <Spacer y={4} />
+      <div className="grid grid-cols-2">
+        <FastboardTableDraggeable />
+        <FastboardTableDraggeable2 />
+      </div>
     </motion.div>
   );
 }
