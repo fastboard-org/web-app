569d3d14a86e4db72f40e2fe7e81c965bff065b9
Simon Stein <sstein@fi.uba.ar>
2024-06-25 00:32:25 -0300

    feat: redirect to connection view on connection card click

--- src/app/home/connections/page.tsx
+++ src/app/home/connections/page.tsx
@@ -2,11 +2,17 @@
 import useConnections from "@/hooks/useConnections";
 import ViewConnections from "@/components/connections/view/ViewConnections";
 import { Connection } from "@/types/connections";
+import { useRouter } from "next/navigation";
 
 export default function Connections() {
   const { connections, loading } = useConnections();
+  const router = useRouter();
 
-  const handleConnectionClick = (connection: Connection) => {};
+  const handleConnectionClick = (connection: Connection) => {
+    setTimeout(() => {
+      router.push(`/home/connections/${connection.id}`);
+    }, 250);
+  };
 
   return (
     <ViewConnections
