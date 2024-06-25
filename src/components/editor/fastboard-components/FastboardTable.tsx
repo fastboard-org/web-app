305dbe922d2e6f4099f9dae4cfd9a15cf00d0d1e
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-25 01:28:41 -0300

    fix: bug when there is no data

--- src/components/editor/fastboard-components/FastboardTable.tsx
+++ src/components/editor/fastboard-components/FastboardTable.tsx
@@ -69,7 +69,7 @@ export default function FastboardTable(props: FastboardTableProps) {
   }
 
   return (
-    <CustomSkeleton isLoaded={!isLoading} className="w-full">
+    <CustomSkeleton isLoaded={!isLoading} onlyRenderOnLoad className="w-full">
       <Table
         isHeaderSticky
         classNames={{
