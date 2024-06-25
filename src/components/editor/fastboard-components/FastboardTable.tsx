c9ea801cbf7149979e47a92cbbb45a5d0f9390ba
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-25 00:11:53 -0300

    update: fix comments

--- src/components/editor/fastboard-components/FastboardTable.tsx
+++ src/components/editor/fastboard-components/FastboardTable.tsx
@@ -14,6 +14,7 @@ import {
   TableRow,
   getKeyValue,
 } from "@nextui-org/react";
+import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 
 export class FastboardTableProperties {
@@ -55,85 +56,69 @@ export default function FastboardTable(props: FastboardTableProps) {
   const { keys, items, isLoading, error, page, setPage, pages, updateQuery } =
     usePaginatedData(4);
 
-  updateQuery(query.url, query.field);
+  useEffect(() => {
+    updateQuery(query.url, query.field);
+  }, [query?.url, query?.field]);
 
   if (error) {
     return (
       <Card className="flex flex-col w-full h-[30%] p-5 justify-center items-center">
-        <h1 className="text-xl text-danger">Failed loading data</h1>
+        <p className="text-xl text-danger">Failed loading data</p>
       </Card>
     );
   }
 
   return (
-    <>
-      {isLoading ? (
-        <Card className="flex flex-col w-full h-[30%] p-5">
-          <div className="flex flex-row">
-            <CustomSkeleton className="w-full rounded-lg">
-              <Spinner />
-            </CustomSkeleton>
-            <Spacer x={2} />
-            <CustomSkeleton className="w-full rounded-lg">
-              <Spinner />
-            </CustomSkeleton>
+    <CustomSkeleton isLoaded={!isLoading} className="w-full">
+      <Table
+        isHeaderSticky
+        classNames={{
+          thead: "-z-10",
+        }}
+        hideHeader={hideHeader}
+        isStriped={isStriped}
+        aria-label="Example table with dynamic content"
+        onClick={(e) => {
+          e.preventDefault();
+          setIsComponentsDrawerOpen(false);
+          setIsPropertiesDrawerOpen(true);
+          props.onClick();
+        }}
+        bottomContent={
+          <div className="flex w-full justify-center">
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              page={page}
+              total={pages}
+              onChange={(page) => setPage(page)}
+            />
           </div>
-          <Spacer y={2} />
-          <CustomSkeleton className="w-full h-full rounded-lg">
-            <Spinner />
-          </CustomSkeleton>
-        </Card>
-      ) : (
-        <Table
-          isHeaderSticky
-          classNames={{
-            thead: "-z-10",
-          }}
-          hideHeader={hideHeader}
-          isStriped={isStriped}
-          aria-label="Example table with dynamic content"
-          onClick={(e) => {
-            e.preventDefault();
-            setIsComponentsDrawerOpen(false);
-            setIsPropertiesDrawerOpen(true);
-            props.onClick();
-          }}
-          bottomContent={
-            <div className="flex w-full justify-center">
-              <Pagination
-                isCompact
-                showControls
-                showShadow
-                page={page}
-                total={pages}
-                onChange={(page) => setPage(page)}
-              />
-            </div>
-          }
+        }
+      >
+        <TableHeader columns={keys}>
+          {(column) => (
+            <TableColumn key={column.key}>
+              {column.label.toUpperCase()}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          isLoading={isLoading}
+          loadingContent={<Spinner label="Loading..." />}
+          items={items}
+          emptyContent={"No rows to display."}
         >
-          <TableHeader columns={keys}>
-            {(column) => (
-              <TableColumn key={column.key}>
-                {column.label.toUpperCase()}
-              </TableColumn>
-            )}
-          </TableHeader>
-          <TableBody
-            isLoading={isLoading}
-            loadingContent={<Spinner label="Loading..." />}
-            items={items}
-            emptyContent={"No rows to display."}
-          >
-            {(item) => (
-              <TableRow key={item.key}>
-                {(columnKey) => (
-                  <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                )}
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      )}
-    </>
+          {(item) => (
+            <TableRow key={item.key}>
+              {(columnKey) => (
+                <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </CustomSkeleton>
   );
 }
