6a1d702e01af3f6c68d5acf907782d09d6388de0
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-06-26 12:52:46 -0300

    update: change types

--- src/atoms/editor/index.ts
+++ src/atoms/editor/index.ts
@@ -27,13 +27,13 @@ export const propertiesDrawerState = atom<PropertiesDrawerState>({
 
 const localStorageEffect =
   (key: string) =>
-  ({ setSelf, onSet }) => {
+  ({ setSelf, onSet }: { setSelf: any; onSet: any }) => {
     const savedValue = localStorage.getItem(key);
     if (savedValue != null) {
       setSelf(JSON.parse(savedValue));
     }
 
-    onSet((newValue, _, isReset) => {
+    onSet((newValue: DashboardMetadata, _: any, isReset: boolean) => {
       isReset
         ? localStorage.removeItem(key)
         : localStorage.setItem(key, JSON.stringify(newValue));
