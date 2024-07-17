d5bc83affb26a4827f5a254d8f70b62c409ae422
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-07-17 15:53:17 -0300

    feat: get queries from back on useQueries hook

--- src/components/editor/QuerySelector.tsx
+++ src/components/editor/QuerySelector.tsx
@@ -2,9 +2,10 @@ import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
 import ConnectionIcon from "../shared/ConnectionIcon";
 import { ConnectionType } from "@/types/connections";
 import { Key } from "react";
+import useQueries from "@/hooks/useQueries";
+import CustomSkeleton from "../shared/CustomSkeleton";
 
 export default function QuerySelector({
-  queries,
   selectedQueryId,
   onSelectionChange,
 }: {
@@ -12,44 +13,54 @@ export default function QuerySelector({
   selectedQueryId: string;
   onSelectionChange: (key: Key) => void;
 }) {
-  const selectedQuery = queries.find((q) => q.id === selectedQueryId);
+  const { queries, loading, isError, error } = useQueries("");
+
+  if (isError) {
+    return <div>Error: {error?.message}</div>;
+  }
+
+  const selectedQuery = queries?.find((q) => q.id === selectedQueryId);
 
   return (
-    <Autocomplete
-      aria-label="Query selector"
-      allowsCustomValue
-      defaultItems={queries}
-      disabledKeys={queries
-        .filter((q) => q.connection.type !== ConnectionType.REST)
-        .map((q) => q.id)}
-      defaultSelectedKey={queries[0]?.id}
-      selectedKey={selectedQuery?.id}
-      label="Query"
-      labelPlacement="outside"
-      placeholder="Select query"
-      startContent={
-        <ConnectionIcon
-          type={selectedQuery?.connection.type}
-          size={25}
-          className="text-primary"
-        />
-      }
-      onSelectionChange={onSelectionChange}
-    >
-      {(query) => (
-        <AutocompleteItem
-          key={query.id}
-          startContent={
-            <ConnectionIcon
-              type={query.connection.type}
-              size={20}
-              className={"text-primary"}
-            />
-          }
-        >
-          {query.name}
-        </AutocompleteItem>
-      )}
-    </Autocomplete>
+    <CustomSkeleton isLoaded={!loading} loadingClassName="h-10 rounded-lg">
+      <Autocomplete
+        aria-label="Query selector"
+        allowsCustomValue
+        defaultItems={queries}
+        disabledKeys={queries
+          ?.filter((q) => q.metadata.type !== ConnectionType.REST)
+          .map((q) => q.id)}
+        defaultSelectedKey={selectedQuery?.id}
+        selectedKey={selectedQuery?.id}
+        label="Query"
+        labelPlacement="outside"
+        placeholder="Select query"
+        startContent={
+          <ConnectionIcon
+            type={selectedQuery?.metadata.type}
+            size={25}
+            className="text-primary"
+          />
+        }
+        onSelectionChange={onSelectionChange}
+        errorMessage="Something went wrong, queries not found"
+        isInvalid={isError}
+      >
+        {(query) => (
+          <AutocompleteItem
+            key={query.id}
+            startContent={
+              <ConnectionIcon
+                type={query.metadata.type}
+                size={20}
+                className={"text-primary"}
+              />
+            }
+          >
+            {query.name}
+          </AutocompleteItem>
+        )}
+      </Autocomplete>
+    </CustomSkeleton>
   );
 }
--- src/hooks/useQueries.ts
+++ src/hooks/useQueries.ts
@@ -1,81 +1,42 @@
-import { useEffect, useState } from "react";
 import { Query } from "@/types/connections";
-
-const mockQueries: Query[] = [
-  {
-    id: "1",
-    name: "Pokemons",
-    connection_id: "1",
-    metadata: {
-      method: "GET",
-      path: "/pokemon",
-    },
-  },
-  {
-    id: "2",
-    name: "Products",
-    connection_id: "1",
-    metadata: {
-      method: "POST",
-    },
-  },
-  {
-    id: "3",
-    name: "Orders",
-    connection_id: "1",
-    metadata: {
-      method: "GET",
-    },
-  },
-  {
-    id: "4",
-    name: "Categories",
-    connection_id: "1",
-    metadata: {
-      method: "GET",
-    },
-  },
-  {
-    id: "5",
-    name: "Customers",
-    connection_id: "1",
-    metadata: {
-      method: "PUT",
-    },
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/lib/axios";
 
 const useQueries = (connectionId: string) => {
-  const [queries, setQueries] = useState<Query[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [mounted, setMounted] = useState(true);
+  const {
+    isPending: loading,
+    isError,
+    data: queries,
+    error,
+  } = useQuery({
+    queryKey: ["queries"],
+    queryFn: () => fetchQueries(),
+    refetchOnWindowFocus: false,
+  });
 
   const fetchQueries = async () => {
-    // TODO: fetch connection by id
-    setTimeout(() => {
-      setQueries(mockQueries.filter((q) => q.connection_id === connectionId));
-      setLoading(false);
-    }, 1000);
+    try {
+      const response = await axiosInstance.get<Query[]>(`/queries/me`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   };
 
-  useEffect(() => {
-    if (!mounted) {
-      return setMounted(true);
-    }
-
-    fetchQueries();
-  }, [connectionId]);
-
   const updateQuery = (queryIndex: number, updatedQuery: Query) => {
+    /*
     const updatedQueries = [...queries];
     updatedQueries[queryIndex] = updatedQuery;
-    setQueries(updatedQueries);
+    setQueries(updatedQueries);*/
   };
 
   return {
     queries,
     loading,
     updateQuery,
+    isError,
+    error,
   };
 };
 
