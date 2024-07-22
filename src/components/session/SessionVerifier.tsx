e93d8fcb4b48b546edd8763d497175ae8d26e0d7
stein257 <sstein@fi.uba.ar>
2024-07-21 23:23:59 -0300

    update: turn SessionVerifier into a typescript component

--- src/components/session/SessionVerifier.tsx
+++ src/components/session/SessionVerifier.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../lib/auth";
+import { auth } from "@/lib/auth";
 import { Spinner } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-const SessionVerifier = ({ children }) => {
+const SessionVerifier = ({ children }: { children: React.ReactNode }) => {
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
   const [mounted, setMounted] = useState(false);
