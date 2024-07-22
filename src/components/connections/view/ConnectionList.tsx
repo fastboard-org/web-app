c7df19552d0f936092fe5822387e1260ac291faf
stein257 <sstein@fi.uba.ar>
2024-07-21 23:23:20 -0300

    feat: implement connection edit/delete logic

--- src/components/connections/view/ConnectionList.tsx
+++ src/components/connections/view/ConnectionList.tsx
@@ -5,13 +5,25 @@ const ConnectionList = ({
   connections,
   search,
   onConnectionClick,
+  onConnectionAction,
 }: {
   connections: Connection[];
   search: string;
   onConnectionClick: (connection: Connection) => void;
+  onConnectionAction: (
+    connection: Connection,
+    action: "edit" | "delete",
+  ) => void;
 }) => {
   return (
     <section className={"flex flex-wrap w-full h-full gap-10 mt-5 "}>
+      {connections.length === 0 && (
+        <p
+          className={"text-center w-full text-xl text-foreground-400 mt-[15%]"}
+        >
+          You don't have any connections yet.
+        </p>
+      )}
       {connections
         .filter((connection) =>
           connection.name.toLowerCase().includes(search.toLowerCase()),
@@ -21,6 +33,9 @@ const ConnectionList = ({
             key={connection.id}
             connection={connection}
             onClick={onConnectionClick}
+            onAction={(action: "edit" | "delete") => {
+              onConnectionAction(connection, action);
+            }}
           />
         ))}
     </section>
