9c88d8a34467db5ca6ff418e9315331528265a89
stein257 <sstein@fi.uba.ar>
2024-07-21 23:14:33 -0300

    feat: add connections service

--- src/components/session/SessionVerifier.tsx
+++ src/components/session/SessionVerifier.tsx
@@ -2,20 +2,25 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../lib/auth";
 import { Spinner } from "@nextui-org/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const SessionVerifier = ({ children }) => {
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
+    if (!mounted) {
+      return setMounted(true);
+    }
+
     if (!user && !loading) {
       router.push("/auth/login");
     }
-  }, [user, loading]);
+  }, [user, loading, mounted]);
 
-  if (!user) {
+  if (!user || !mounted) {
     return (
       <div className={"w-screen h-screen flex justify-center items-center"}>
         <Spinner />
