181dce97902b1c8655c8b4e4d405c3fc04b871ef
unknown <tgrodriguez@fi.uba.ar>
2024-07-30 16:01:54 -0300

    update: Fixed layouts and added padding instead of y and x space

--- src/components/editor/layouts/BottomSplitLayout.tsx
+++ src/components/editor/layouts/BottomSplitLayout.tsx
@@ -10,32 +10,35 @@ export default function BottomSplitLayout({
   properties: BottomSplitLayoutInterface;
   mode?: "editable" | "view";
 }) {
-    return (
-        <div className="flex flex-col -space-y-12 h-full w-full">
-        <Container
+  return (
+    <div className="flex flex-col h-full w-full">
+      <Container
+        layoutIndex={index}
+        containerIndex="component1"
+        component={properties.component1}
+        mode={mode}
+        className="px-5 pt-5 pb-2 h-1/2"
+      />
+      <div className="flex flex-row h-1/2">
+        <div className="w-1/2">
+          <Container
             layoutIndex={index}
-            containerIndex="component1"
-            component={properties.component1}
+            containerIndex="component2"
+            component={properties.component2}
             mode={mode}
-        />
-        <div className="flex flex-row -space-x-12 h-full">
-            <div className="w-1/2">
-              <Container
-              layoutIndex={index}
-              containerIndex="component2"
-              component={properties.component2}
-              mode={mode}
-              />
-            </div>
-            <div className="w-1/2">
-              <Container
-              layoutIndex={index}
-              containerIndex="component3"
-              component={properties.component3}
-              mode={mode}
-              />
-            </div>
+            className="pl-5 pr-1 pt-1 pb-5 h-full"
+          />
         </div>
+        <div className="w-1/2">
+          <Container
+            layoutIndex={index}
+            containerIndex="component3"
+            component={properties.component3}
+            mode={mode}
+            className="pr-5 pl-1 pt-1 pb-5 h-full"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/editor/layouts/RightSplitLayout.tsx
+++ src/components/editor/layouts/RightSplitLayout.tsx
@@ -1,4 +1,4 @@
-import { RightSplitLayout as RightSplitLayoutInterface} from "@/types/editor";
+import { RightSplitLayout as RightSplitLayoutInterface } from "@/types/editor";
 import Container from "./Container";
 
 export default function RightSplitLayout({
@@ -10,30 +10,33 @@ export default function RightSplitLayout({
   properties: RightSplitLayoutInterface;
   mode?: "editable" | "view";
 }) {
-    return (
-        <div className="flex flex-row h-full w-full">
-            <div className="w-1/2">
-                <Container
-                    layoutIndex={index}
-                    containerIndex="component1"
-                    component={properties.component1}
-                    mode={mode}
-                />
-            </div>
-            <div className="flex flex-col w-1/2 h-full -space-y-12">
-                <Container
-                    layoutIndex={index}
-                    containerIndex="component2"
-                    component={properties.component2}
-                    mode={mode}
-                />
-                <Container
-                    layoutIndex={index}
-                    containerIndex="component3"
-                    component={properties.component3}
-                    mode={mode}
-                />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="flex flex-row h-full w-full">
+      <div className="w-1/2 h-full">
+        <Container
+          layoutIndex={index}
+          containerIndex="component1"
+          component={properties.component1}
+          mode={mode}
+          className="p-5"
+        />
+      </div>
+      <div className="flex flex-col w-1/2 h-full">
+        <Container
+          layoutIndex={index}
+          containerIndex="component2"
+          component={properties.component2}
+          mode={mode}
+          className="pl-1 pr-5 pt-5 pb-1 h-1/2"
+        />
+        <Container
+          layoutIndex={index}
+          containerIndex="component3"
+          component={properties.component3}
+          mode={mode}
+          className="pl-1 pr-5 pt-1 pb-5 h-1/2"
+        />
+      </div>
+    </div>
+  );
+}
