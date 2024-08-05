2e2251025fae83ee07a81885468a53cf6f53e8c6
unknown <tgrodriguez@fi.uba.ar>
2024-08-05 16:45:16 -0300

    feat: Added cardPerRow prop

--- src/components/editor/fastboard-components/cards/CustomCard.tsx
+++ src/components/editor/fastboard-components/cards/CustomCard.tsx
@@ -6,9 +6,11 @@ import {
   Divider,
 } from "@nextui-org/react";
 import CardEntry from "./CardEntry";
+import scrollbarStyles from "@/styles/scrollbar.module.css";
 
 const CustomCard = ({
   data,
+  cardsPerRow,
   className,
 }: {
   data: {
@@ -16,27 +18,29 @@ const CustomCard = ({
     footer: string;
     body: { key: string; value: string }[];
   };
+  cardsPerRow: number;
   className?: string;
 }) => {
   const headerText = data.header;
   const footerText = data.footer;
   const bodyData = data.body;
-
+  const cardWidth = Math.floor(100 / (cardsPerRow + 1));
+  const customClassName = `flex px-2 py-2 h-80 w-[${cardWidth}%] 
+  )}%]`;
   return (
-    <div className="flex grow-0 h-fit min-h-full w-1/4 px-5 py-2 border-4 border-green-500">
+    <div className={customClassName}>
       <Card
         aria-label="Fastboard cards draggable"
-        isFooterBlurred
-        // className={`h-full ${className}`}
-        className="grow-0 h-full w-full max-h-full border-4 border-white"
+        // isFooterBlurred
+        className="grow-0 h-full w-full max-h-full"
       >
-        <CardHeader className="shrink h-1/5 w-full flex justify-center items-center text-medium border-4 border-yellow-500">
+        <CardHeader className="shrink h-1/5 w-full flex justify-center items-center text-medium">
           {headerText}
         </CardHeader>
         <Divider />
         <CardBody
           aria-label="Fastboard cards draggable body"
-          className="shrink h-4/6 text-small border-4 border-red-500"
+          className={`shrink h-4/6 text-small ${scrollbarStyles.scrollbar}`}
         >
           <div className="flex flex-col">
             {bodyData.map((item, index) => (
@@ -45,7 +49,7 @@ const CustomCard = ({
           </div>
         </CardBody>
         <Divider />
-        <CardFooter className="shrink h-1/6 flex justify-start items-center text-tiny border-4 border-blue-500">
+        <CardFooter className="shrink h-1/6 flex justify-start items-center text-tiny">
           {footerText}
         </CardFooter>
       </Card>
