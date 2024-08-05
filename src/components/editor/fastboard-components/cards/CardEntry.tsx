b18507e386592272ccb20822208f27249a92cfc6
unknown <tgrodriguez@fi.uba.ar>
2024-08-05 16:44:50 -0300

    update: Changed background color

--- src/components/editor/fastboard-components/cards/CardEntry.tsx
+++ src/components/editor/fastboard-components/cards/CardEntry.tsx
@@ -1,6 +1,6 @@
 const CardEntry = ({ entry }: { entry: { key: string; value: string } }) => {
   return (
-    <div className="p-2 my-1 bg-[#27272A]/100 rounded-md shadow-lg shadow-black/25 rounded">
+    <div className="p-2 my-1 bg-content2 rounded-md shadow-lg shadow-black/25 rounded">
       <p>
         <span className="font-bold">{entry.key}:</span> {entry.value}
       </p>
