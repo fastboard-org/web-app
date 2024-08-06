5f3554abeb34b547381046b635390f73e3f9354d
unknown <tgrodriguez@fi.uba.ar>
2024-08-06 00:30:19 -0300

    update: Fixed draggable cards height and removed comment

--- src/components/editor/fastboard-components/cards/FastboardCardsDraggable.tsx
+++ src/components/editor/fastboard-components/cards/FastboardCardsDraggable.tsx
@@ -23,62 +23,36 @@ export default function FastboardCardsDraggable() {
         }}
         dragSnapToOrigin
       >
-        <div className="flex flex-row w-full justify-between">
+        <div className="flex flex-row overflow-hidden w-full justify-between">
           <CustomCard
-            cardsPerRow={2}
             data={{
               header: "Header",
               footer: "Footer",
-              body: [
-                { key: "key1", value: "value1" },
-                { key: "key2", value: "value2" },
-              ],
+              body: [{ key: "key", value: "value" }],
             }}
+            cardsPerRow={3}
+            height="160px"
           />
           <CustomCard
-            cardsPerRow={2}
             data={{
               header: "Header",
               footer: "Footer",
-              body: [
-                { key: "key1", value: "value1" },
-                { key: "key2", value: "value2" },
-              ],
+              body: [{ key: "key", value: "value" }],
             }}
+            cardsPerRow={3}
+            height="160px"
           />
           <CustomCard
-            cardsPerRow={2}
             data={{
               header: "Header",
               footer: "Footer",
-              body: [
-                { key: "key1", value: "value1" },
-                { key: "key2", value: "value2" },
-              ],
+              body: [{ key: "key", value: "value" }],
             }}
+            cardsPerRow={3}
+            height="160px"
           />
         </div>
       </Draggable>
     </div>
   );
-}
-
-{
-  /* <Card css={{ width: '300px', height: '400px', padding: '0' }}>
-      <CardHeader css={{ height: '20%', justifyContent: 'center', alignItems: 'center' }}>
-        <Text size="$md" css={{ textAlign: 'center' }}>{headerText}</Text>
-      </CardHeader>
-      <Divider />
-      <CardBody css={{ height: '65%', overflowY: 'auto' }}>
-        {bodyData.map((item, index) => (
-          <Text key={index} size={`calc(1.5rem - ${bodyData.length * 0.1}rem)`}>
-            <b>{item.key}:</b> {item.value}
-          </Text>
-        ))}
-      </CardBody>
-      <Divider />
-      <CardFooter css={{ height: '15%', justifyContent: 'flex-start', alignItems: 'center' }}>
-        <Text size="$xs">{footerText}</Text>
-      </CardFooter>
-    </Card> */
 }
