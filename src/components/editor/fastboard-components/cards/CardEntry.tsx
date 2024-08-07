1ab1c13302c7d6773caa4b71059c77341aa49459
unknown <tgrodriguez@fi.uba.ar>
2024-08-06 22:45:00 -0300

    update: Removed shadow on CardEntry

--- src/components/editor/fastboard-components/cards/CardEntry.tsx
+++ src/components/editor/fastboard-components/cards/CardEntry.tsx
@@ -1,6 +1,6 @@
 const CardEntry = ({ entry }: { entry: { key: string; value: string } }) => {
   return (
-    <div className="p-2 my-1 bg-content2 rounded-md shadow-lg shadow-black/25">
+    <div className="p-2 my-1 bg-content2 rounded-md">
       <p>
         <span className="font-bold">{entry.key}:</span> {entry.value}
       </p>
