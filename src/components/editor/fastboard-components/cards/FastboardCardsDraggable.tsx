fc679a722b4f1d9cf513479e7f717822b208d4f6
Simón Stein <86434696+stein257@users.noreply.github.com>
2024-08-09 18:42:17 -0300

    Add draggable svgs (#47)
    * feat: add draggables svgs
    
    * feat: create DraggableImage component
    
    * feat: use DraggableImage in draggable components
    
    * feat: extract container styles into Draggable component
    
    * update: cards svg

--- src/components/editor/fastboard-components/cards/FastboardCardsDraggable.tsx
+++ src/components/editor/fastboard-components/cards/FastboardCardsDraggable.tsx
@@ -1,58 +1,20 @@
 import Draggable from "../Draggable";
 import { ComponentType } from "@/types/editor";
 import { FastboardCardsProperties } from "@/types/editor/cards-types";
-import CustomCard from "./CustomCard";
-
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Divider,
-} from "@nextui-org/react";
+import DraggableImage from "@/components/shared/DraggableImage";
 
 export default function FastboardCardsDraggable() {
   return (
-    <div className="flex flex-col justify-center">
-      <h4 className={"text-md pb-2"}>Cards</h4>
-      <Draggable
-        id="cards-draggable"
-        data={{
-          type: ComponentType.Cards,
-          defaultProperties: FastboardCardsProperties.default(),
-        }}
-        dragSnapToOrigin
-      >
-        <div className="flex flex-row overflow-hidden w-full justify-between">
-          <CustomCard
-            data={{
-              header: "Header",
-              footer: "Footer",
-              body: [{ key: "key", value: "value" }],
-            }}
-            cardsPerRow={3}
-            height="160px"
-          />
-          <CustomCard
-            data={{
-              header: "Header",
-              footer: "Footer",
-              body: [{ key: "key", value: "value" }],
-            }}
-            cardsPerRow={3}
-            height="160px"
-          />
-          <CustomCard
-            data={{
-              header: "Header",
-              footer: "Footer",
-              body: [{ key: "key", value: "value" }],
-            }}
-            cardsPerRow={3}
-            height="160px"
-          />
-        </div>
-      </Draggable>
-    </div>
+    <Draggable
+      id="cards-draggable"
+      data={{
+        type: ComponentType.Cards,
+        defaultProperties: FastboardCardsProperties.default(),
+      }}
+      dragSnapToOrigin
+      name={"Cards"}
+    >
+      <DraggableImage name={"cards"} alt={"Cards"} />
+    </Draggable>
   );
 }
