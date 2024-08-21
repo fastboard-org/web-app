62dd379adaba0dc79c2640b712f0d09e5d3e3bbd
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-08-21 19:34:03 -0300

    update: include home button and reorder theme switcher

--- src/components/editor/EditorNavbar.tsx
+++ src/components/editor/EditorNavbar.tsx
@@ -1,18 +1,27 @@
-import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import {
+  Button,
+  Link,
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
 import PublishButton from "./PublishButton";
 import DashboardName from "./DashboardName";
 import EditorMenu from "./EditorMenu";
 import { ThemeSwitcher } from "../layout/ThemeSwitcher";
+import Logo from "../layout/shared/Logo";
 
 export default function EditorNavbar() {
   return (
     <Navbar maxWidth="full" className="bg-background shadow p-0.5">
       <NavbarContent justify="start">
         <NavbarItem>
-          <EditorMenu />
+          <Button as={Link} href="/home/dashboards" isIconOnly variant="light">
+            <Logo classname="text-primary" />
+          </Button>
         </NavbarItem>
         <NavbarItem>
-          <ThemeSwitcher size="md" />
+          <EditorMenu />
         </NavbarItem>
       </NavbarContent>
 
@@ -25,6 +34,9 @@ export default function EditorNavbar() {
       <NavbarContent justify="end">
         <NavbarItem>
           <PublishButton />
+        </NavbarItem>
+        <NavbarItem>
+          <ThemeSwitcher size="md" />
         </NavbarItem>
       </NavbarContent>
     </Navbar>
