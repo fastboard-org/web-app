6f9c1c22ab60e763c2b457e922a5f2a97f22e0d1
Chiki <66981070+JulianBiancardi@users.noreply.github.com>
2024-09-16 15:29:44 -0300

    update: log out popover

--- src/components/shared/FastboardAvatar.tsx
+++ src/components/shared/FastboardAvatar.tsx
@@ -1,9 +1,36 @@
-import { auth } from "@/lib/auth";
-import { Avatar } from "@nextui-org/react";
+import { auth, signOut } from "@/lib/auth";
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { LoginCurve } from "iconsax-react";
+import { useRouter } from "next/navigation";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function FastboardAvatar() {
+  const router = useRouter();
   const [user, error] = useAuthState(auth);
 
-  return <Avatar showFallback src={user?.photoURL as string}></Avatar>;
+  return (
+    <Popover showArrow placement="bottom">
+      <PopoverTrigger>
+        <Avatar as="button" showFallback src={user?.photoURL as string} />
+      </PopoverTrigger>
+      <PopoverContent className="p-1">
+        <Button
+          variant="light"
+          endContent={<LoginCurve size="24" className="text-default-600" />}
+          onClick={async () => {
+            await signOut();
+            router.push("/auth/login");
+          }}
+        >
+          Log out
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
 }
