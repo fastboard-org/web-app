b2ae70021d270ef3f315b30334473e366a0f4fee
stein257 <sstein@fi.uba.ar>
2024-11-03 23:46:38 -0300

    feat: only reset properties if click comes from other component

--- src/components/editor/fastboard-components/FastboardComponent.tsx
+++ src/components/editor/fastboard-components/FastboardComponent.tsx
@@ -6,109 +6,115 @@ import {
   propertiesDrawerState,
 } from "@/atoms/editor";
 import { ComponentId, ComponentType, Context } from "@/types/editor";
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { getComponent } from "./utils";
 import { Button } from "@nextui-org/react";
 import { Trash } from "iconsax-react";
 import useDashboard from "@/hooks/dashboards/useDashboard";
 
-const FastboardComponent = ({
-  id,
-  name,
-  type,
-  properties,
-  context,
-  mode = "editable",
-  canDelete = true,
-  onClick,
-}: {
-  id: ComponentId;
-  name: string;
-  type: ComponentType;
-  properties: Record<string, any>;
-  context: Context;
-  mode?: "editable" | "view";
-  canDelete?: boolean;
-  onClick?: () => void;
-}) => {
-  const { deleteComponentFromLayout } = useDashboard();
-  const setIsComponentsDrawerOpen = useSetRecoilState(isComponentsDrawerOpen);
-  const setIsSettingsDrawerOpen = useSetRecoilState(isSettingsDrawerOpen);
-  const setIsAuthDrawerOpen = useSetRecoilState(isAuthDrawerOpen);
-  const [propertiesDrawerOpen, setIsPropertiesDrawerOpen] = useRecoilState(
-    isPropertiesDrawerOpen
-  );
-  const [propertiesDrawerStateValue, setPropertiesDrawerState] = useRecoilState(
-    propertiesDrawerState
-  );
-  const [isHovered, setIsHovered] = useState(false);
+const FastboardComponent = memo(
+  ({
+    id,
+    name,
+    type,
+    properties,
+    context,
+    mode = "editable",
+    canDelete = true,
+    onClick,
+  }: {
+    id: ComponentId;
+    name: string;
+    type: ComponentType;
+    properties: Record<string, any>;
+    context: Context;
+    mode?: "editable" | "view";
+    canDelete?: boolean;
+    onClick?: () => void;
+  }) => {
+    const { deleteComponentFromLayout } = useDashboard();
+    const setIsComponentsDrawerOpen = useSetRecoilState(isComponentsDrawerOpen);
+    const setIsSettingsDrawerOpen = useSetRecoilState(isSettingsDrawerOpen);
+    const setIsAuthDrawerOpen = useSetRecoilState(isAuthDrawerOpen);
+    const [propertiesDrawerOpen, setIsPropertiesDrawerOpen] = useRecoilState(
+      isPropertiesDrawerOpen,
+    );
+    const [propertiesDrawerStateValue, setPropertiesDrawerState] =
+      useRecoilState(propertiesDrawerState);
+    const [isHovered, setIsHovered] = useState(false);
 
-  function onClickComponent(e: React.MouseEvent<HTMLDivElement>) {
-    setIsComponentsDrawerOpen(false);
-    setIsSettingsDrawerOpen(false);
-    setIsAuthDrawerOpen(false);
-    setIsPropertiesDrawerOpen(true);
-    setPropertiesDrawerState({
-      selectedComponentId: id,
-      type: type,
-      properties: properties,
-      context: context,
-    });
-    if (onClick) {
-      onClick();
+    function onClickComponent(e: React.MouseEvent<HTMLDivElement>) {
+      setIsComponentsDrawerOpen(false);
+      setIsSettingsDrawerOpen(false);
+      setIsAuthDrawerOpen(false);
+      setIsPropertiesDrawerOpen(true);
+
+      const sameComponent =
+        propertiesDrawerStateValue.selectedComponentId === id;
+      if (sameComponent) return;
+
+      setPropertiesDrawerState({
+        selectedComponentId: id,
+        type: type,
+        properties: properties,
+        context: context,
+      });
+      if (onClick) {
+        onClick();
+      }
     }
-  }
 
-  function onDeleteComponent() {
-    if (!context.layoutContext) return null;
-    setIsPropertiesDrawerOpen(false);
-    deleteComponentFromLayout(context.layoutContext);
-  }
+    function onDeleteComponent() {
+      if (!context.layoutContext) return null;
+      setIsPropertiesDrawerOpen(false);
+      deleteComponentFromLayout(context.layoutContext);
+    }
 
-  function isSelected() {
-    if (
-      propertiesDrawerOpen &&
-      propertiesDrawerStateValue.selectedComponentId === id
-    )
-      return true;
+    function isSelected() {
+      if (
+        propertiesDrawerOpen &&
+        propertiesDrawerStateValue.selectedComponentId === id
+      )
+        return true;
 
-    return isHovered;
-  }
+      return isHovered;
+    }
 
-  const component = getComponent(type, id, mode, properties);
-  if (!component) return null;
+    const component = getComponent(type, id, mode, properties);
+    if (!component) return null;
 
-  if (mode === "view") {
-    return component;
-  }
+    if (mode === "view") {
+      return component;
+    }
 
-  return (
-    <div
-      className={`relative w-full h-full transition border-2  cursor-pointer ${
-        isSelected() ? "border-primary" : "border-transparent"
-      } ${
-        type === ComponentType.Header || type === ComponentType.Sidebar
-          ? "rounded-none"
-          : "rounded-2xl"
-      }`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onClick={onClickComponent}
-    >
-      {isSelected() && canDelete && (
-        <Button
-          isIconOnly
-          className="absolute bottom-2 left-2 z-20 text-danger"
-          variant="light"
-          onPress={onDeleteComponent}
-        >
-          <Trash />
-        </Button>
-      )}
-      {component}
-    </div>
-  );
-};
+    return (
+      <div
+        className={`relative w-full h-full transition border-2  cursor-pointer ${
+          isSelected() ? "border-primary" : "border-transparent"
+        } ${
+          type === ComponentType.Header || type === ComponentType.Sidebar
+            ? "rounded-none"
+            : "rounded-2xl"
+        }`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={onClickComponent}
+      >
+        {isSelected() && canDelete && (
+          <Button
+            isIconOnly
+            className="absolute bottom-2 left-2 z-20 text-danger"
+            variant="light"
+            onPress={onDeleteComponent}
+          >
+            <Trash />
+          </Button>
+        )}
+        {component}
+      </div>
+    );
+  },
+);
 
 export default FastboardComponent;
