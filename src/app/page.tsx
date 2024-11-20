ddee2aa09031a617247afde72abb5b22179982b9
Simón Stein <86434696+stein257@users.noreply.github.com>
2024-11-20 11:50:38 -0300

    Landing Page (#83)
    * feet: add landing page
    
    * feat: add FadeUp component
    
    * feat: add screenshot
    
    * feat: add screenshot section in landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,185 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { ArrowDown2, ArrowRight } from "iconsax-react";
+import React, { useState } from "react";
 import Image from "next/image";
+import FadeUp from "@/components/shared/FadeUp";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [firstDelay, setFirstDelay] = useState(0.4);
+  const [secondDelay, setSecondDelay] = useState(0.8);
+  const [showArrow, setShowArrow] = useState(false);
+
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main
+      style={{
+        background:
+          "linear-gradient(0deg, rgba(135,190,252,0.7) 0%, rgba(255,255,255,1) 80%)",
+      }}
+      className="flex flex-col min-h-screen items-center justify-center bg-background overflow-x-hidden !scroll-smooth"
+    >
+      <div
+        className={"flex gap-20 justify-center items-center w-full h-screen"}
+      >
+        <AnimatePresence>
+          {showArrow && (
+            <motion.a
+              href={"#about"}
+              initial={{ opacity: 0, y: 0 }}
+              animate={{
+                opacity: [0, 1],
+                y: [0, 10],
+              }}
+              transition={{
+                opacity: { duration: 1 },
+                y: {
+                  repeat: Infinity,
+                  repeatType: "mirror",
+                  duration: 1,
+                  ease: "easeInOut",
+                },
+              }}
+              className={"absolute bottom-10"}
+            >
+              <ArrowDown2 size={40} className={"text-black cursor-pointer"} />
+            </motion.a>
+          )}
+        </AnimatePresence>
+        <p className={"absolute top-20 text-white z-20 cursor-default"}>
+          Made by @JulianBiancardi, @TGRodriguez & @stein257
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+        <div className={"flex gap-[10px]"}>
+          <motion.div
+            drag
+            dragSnapToOrigin
+            whileDrag={{ scale: 1.1, cursor: "grabbing" }}
+            initial={{ x: -200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.25 }}
+            className={
+              "w-[106px] h-[240px] bg-primary rounded-[10px] cursor-grab"
+            }
+          />
+          <div className={"flex flex-col justify-between "}>
+            <motion.div
+              drag
+              dragSnapToOrigin
+              whileDrag={{ scale: 1.1, cursor: "grabbing" }}
+              initial={{ y: -200, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 0.25,
+                ease: "easeInOut",
+                delay: firstDelay,
+              }}
+              className={
+                "w-[106px] h-[115px] bg-primary rounded-[10px] cursor-grab"
+              }
+              onAnimationComplete={() => setFirstDelay(0)}
             />
-          </a>
+            <motion.div
+              drag
+              dragSnapToOrigin
+              whileDrag={{ scale: 1.1, cursor: "grabbing" }}
+              initial={{ y: 200, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                ease: "easeInOut",
+                duration: 0.25,
+                delay: secondDelay,
+              }}
+              className={
+                "w-[106px] h-[115px] bg-primary rounded-[10px] cursor-grab"
+              }
+              onAnimationComplete={() => setSecondDelay(0)}
+            />
+          </div>
+        </div>
+        <div className={"flex flex-col"}>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 1.2 }}
+            className={
+              "text-8xl font-normal text-center text-black z-10 relative top-0"
+            }
+          >
+            Fastboard
+          </motion.h1>
+          <motion.a
+            initial={{ opacity: 0, x: 10, y: 10 }}
+            animate={{ opacity: 1, x: 0, y: 10 }}
+            transition={{ ease: "easeInOut", duration: 0.25, delay: 1.6 }}
+            href={"/auth/login"}
+            className={
+              "text-xl text-black flex gap-1 items-center self-end hover:underline"
+            }
+            onAnimationComplete={() => setShowArrow(true)}
+          >
+            Start now
+            <ArrowRight size={20} />
+          </motion.a>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={"flex flex-col items-center pt-24"} id={"about"}>
+        <FadeUp delay={0.25}>
+          <h2
+            className={"text-black text-center text-5xl max-w-[1000px] mb-12"}
+          >
+            An easy and intuitive solution for developing dashboards
+          </h2>
+        </FadeUp>
+        <FadeUp delay={0.5}>
+          <p className={"max-w-[700px] text-center mb-10"}>
+            Connect your data sources and start creating your own dashboards
+            with Fastboard. Just as simple as drag and dropping.
+          </p>
+        </FadeUp>
+        <FadeUp delay={0.6}>
+          <Button
+            className={"bg-black text-white rounded-3xl mb-12"}
+            variant={"shadow"}
+            onClick={() => router.push("/auth/login")}
+          >
+            Get started for free
+          </Button>
+        </FadeUp>
+        <FadeUp delay={0.75}>
+          <div
+            className={"w-[100%] rounded-lg shadow-2xl overflow-hidden mb-12"}
+          >
+            <Image
+              src={"/screenshot.png"}
+              alt={"image"}
+              width={1000}
+              height={400}
+              quality={100}
+            />
+          </div>
+        </FadeUp>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer
+        className={
+          "flex items-center justify-center w-full text-center p-5 gap-5 mt-12"
+        }
+      >
+        <p className={"text-black"}>
+          Star us on{" "}
+          <a
+            href={"https://github.com/fastboard-org"}
+            target={"_blank"}
+            className={"hover:underline"}
+          >
+            <b>Github</b>
+          </a>
+        </p>
+        <p>|</p>
+        <p className={"text-black"}>© 2024 Fastboard</p>
+      </footer>
     </main>
   );
 }
